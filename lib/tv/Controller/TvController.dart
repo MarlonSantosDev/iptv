f683f813421651fd48e074a8f303fd7a51aa647d
Marlon Santos <marlon-20-12@hotmail.com>
2023-02-27 10:19:47 -0300

    LIsta de map

--- lib/tv/Controller/TvController.dart
+++ lib/tv/Controller/TvController.dart
@@ -18,12 +18,21 @@ abstract class TvControllerbase with Store {
     final conteuDoArquivo = await parseFile(arquivo);
 
     conteuDoArquivo.map((e) {
-      String valor = e.toString().replaceAll(" ", "");
-      valor = valor.toString().replaceAll("\"", "'");
+      //String valor = e.toString().replaceAll(" ", "");
+      //valor = valor.toString().replaceAll("\"", "'");
 
-      printO(valor);
+      String valor = e.toString().substring(e.toString().indexOf('tvg-id='), e.toString().length);
+      String valor2 = valor.substring(valor.toString().indexOf('group-title='), valor.length);
+      List lista2 = valor2.split(',');
+
+      String map = '${valor.substring(0, valor.indexOf(lista2[0]))}${lista2[0]}';
+
+      String part0 = e.toString().substring(0, e.toString().indexOf('tvg-id='));
+
+      printW("${part0}  ${map} ${lista2[1]}");
+
+      printO(e);
     }).toList();
-
     /*
     Title:  Link:#EXTINF:-1 tvg-id="" tvg-name="24H BREAKING BAD" tvg-logo="http://www.rochagalaxia.live/logos/breakingbad250.png" group-title="CANAIS: 24 HORAS",24H BREAKING BAD hasAttributes:false
     Title:  Link:#EXTINF:-1 tvg-id="" tvg-name="Red Rose S01 Red Rose S01E07" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/jS8mOKloyEzqQKrne0w4Upu1pnv.jpg" group-title="Séries | Netflix",Red Rose S01 Red Rose S01E07 hasAttributes:false
